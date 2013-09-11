431f04fe3d280e57d24fe647e39b7abe255f59b0
Philip Youssef <pyoussef@twitter.com>
2013-09-11 14:16:48 -0700

    Added handler to YambaService for showing confirmation toast to user.

--- Yamba/src/main/java/com/twitter/android/yamba/service/YambaService.java
+++ Yamba/src/main/java/com/twitter/android/yamba/service/YambaService.java
@@ -3,7 +3,10 @@ package com.twitter.android.yamba.service;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.twitter.android.yamba.BuildConfig;
 import com.twitter.android.yamba.R;
@@ -18,9 +21,21 @@ public class YambaService extends IntentService {
     public static final String TAG = "SVC";
 
     private static final String PARAM_STATUS = "YambaService.STAUTS";
-    private static volatile YambaClient client;
-    static {
-         client = new YambaClient("student", "password");
+    private static final int OP_POST_COMPLETE = -1;
+
+    // A handler to send messages back to the UI thread.
+    private Handler mHandler;
+
+    private static YambaClient sClient;
+    private static YambaClient getClient() {
+        if (sClient == null) {
+            synchronized (YambaService.class) {
+                if (sClient == null) {
+                    sClient = new YambaClient("student", "password");
+                }
+            }
+        }
+        return sClient;
     }
 
     public static void post(Context ctxt, String status) {
@@ -31,18 +46,43 @@ public class YambaService extends IntentService {
 
     public YambaService() {
         super(TAG);
+
+        mHandler = new PostHandler(this);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        String postStatus = intent.getStringExtra(PARAM_STATUS);
-        if (BuildConfig.DEBUG) {Log.d(TAG, "posting: " + postStatus);}
-
+        int message = R.string.submit_failed_msg;
         try {
-            client.postStatus(postStatus);
+            String postStatus = intent.getStringExtra(PARAM_STATUS);
+            if (BuildConfig.DEBUG) {Log.d(TAG, "posting: " + postStatus);}
+            getClient().postStatus(postStatus);
+            message = R.string.submit_success_msg;
         } catch (YambaClientException e) {
             e.printStackTrace();
         }
 
+        Message.obtain(mHandler, OP_POST_COMPLETE, message, 0).sendToTarget();
+    }
+
+    protected void onPostComplete(int message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private static class PostHandler extends Handler {
+
+        private YambaService mService;
+
+        public PostHandler(YambaService service) {
+            mService = service;
+        }
+
+        @Override
+        public void dispatchMessage(Message msg) {
+            switch (msg.what) {
+                case OP_POST_COMPLETE:
+                    mService.onPostComplete(msg.arg1);
+            }
+        }
     }
 }
